1c68cd2027f0c120e6bb1dd41761355ddc056367
tkeaton <41343362+tkeaton@users.noreply.github.com>
2018-07-17 12:22:01 -0600

    esphere

---
+++ esphere.m
@@ -0,0 +1,7 @@
+
+%Constants of Nature
+Q = 
+epnaut = 8.854187*(10^-12)
+Ex = (Q*x)/(4*pi*epnaut*(x^2+y^2+z^2)^(3/2));
+Ey = (Q*y)/(4*pi*epnaut*(x^2+y^2+z^2)^(3/2));
+Ez = (Q*z)/(4*pi*epnaut*(x^2+y^2+z^2)^(3/2));
