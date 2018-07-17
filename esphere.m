0e5250d0cec1099f9ead57b7b8328a0921d1d452
tkeaton <41343362+tkeaton@users.noreply.github.com>
2018-07-17 12:40:50 -0600

    Update esphere.m

--- esphere.m
+++ esphere.m
@@ -1,7 +1,13 @@
+function [Ex,Ey,Ez] = esphere(V,x,y,z)
 
-%Constants of Nature
-Q = 
-epnaut = 8.854187*(10^-12)
+%Constants in kg and m
+epnaut = 8.854187*(10^-12);
+k = 1/(4*pi*epnaut);
+R = 0.1;
+Q = (V*R)/k;
+
 Ex = (Q*x)/(4*pi*epnaut*(x^2+y^2+z^2)^(3/2));
 Ey = (Q*y)/(4*pi*epnaut*(x^2+y^2+z^2)^(3/2));
 Ez = (Q*z)/(4*pi*epnaut*(x^2+y^2+z^2)^(3/2));
+
+end
